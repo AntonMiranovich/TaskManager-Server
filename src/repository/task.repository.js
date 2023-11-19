7f9bf1ef209d7bd6750351794860a4c1aa2a41b4
AntonMiranovich <antonmir992@gmail.com>
2023-11-19 13:07:48 +0300

    work midl123

--- src/repository/task.repository.js
+++ src/repository/task.repository.js
@@ -16,36 +16,66 @@ async function getDataTaskByIdDB(id) {
 
 async function createDataDB(task, user_id) {
   const client = await pool.connect();
-  const sql = `insert into tasks (task , user_id)
-values ($1,$2) returning *`;
-  const data = (await client.query(sql, [task, user_id])).rows;
-  return data;
+  try {
+    await client.query('BEGEN');
+    const sql = `insert into tasks (task , user_id)
+  values ($1,$2) returning *`;
+    const data = (await client.query(sql, [task, user_id])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`createDataDB:${error.message}`);
+    return [];
+  }
 }
 
 async function updateTaskByIdDB(id, task, user_id) {
   const client = await pool.connect();
-  const sql = `update tasks set task=$1,user_id=$2 where id=$3 returning *`;
-  const result = (await client.query(sql, [task, user_id, id])).rows;
-  return result;
+  try {
+    await client.query('BEGEN');
+    const sql = `update tasks set task=$1,user_id=$2 where id=$3 returning *`;
+    const result = (await client.query(sql, [task, user_id, id])).rows;
+    await client.query('COMMIT');
+    return result;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`updateTaskByIdDB:${error.message}`);
+    return [];
+  }
 }
 
 async function deleteDataTaskDB(id) {
   const client = await pool.connect();
-  const sql = `delete from tasks where id=$1 returning *`;
-  const data = (await client.query(sql, [id])).rows;
-  return data;
+  try {
+    await client.query('BEGEN');
+    const sql = `delete from tasks where id=$1 returning *`;
+    const data = (await client.query(sql, [id])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`deleteDataTaskDB:${error.message}`);
+    return [];
+  }
 }
 
 async function patchDataTaskDB(id, clientObj) {
   const client = await pool.connect();
-  const sql1 = `select * from tasks where id=$1`;
-  const oldObj = (await client.query(sql1, [id])).rows;
-
-  const newObj = { ...oldObj[0], ...clientObj };
-
-  const sql2 = `update tasks set task=$1,user_id=$2 where id=$3 returning *`;
-  const result = (await client.query(sql2, [newObj.task, newObj.user_id, id])).rows;
-  return result;
+  try {
+    await client.query('BEGEN');
+    const sql1 = `select * from tasks where id=$1`;
+    const oldObj = (await client.query(sql1, [id])).rows;
+    const newObj = { ...oldObj[0], ...clientObj };
+    const sql2 = `update tasks set task=$1,user_id=$2 where id=$3 returning *`;
+    const result = (await client.query(sql2, [newObj.task, newObj.user_id, id])).rows;
+    await client.query('COMMIT');
+    return result;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`patchDataTaskDB:${error.message}`);
+    return [];
+  }
 }
 
 module.exports = { getAllDataDB, createDataDB, deleteDataTaskDB, updateTaskByIdDB, patchDataTaskDB, getDataTaskByIdDB };
--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -7,14 +7,6 @@ async function getAllUsersDB() {
   return data;
 }
 
-async function createDataDB(name, surname, email, pwd) {
-  const client = await pool.connect();
-  const sql = `insert into users (name, surname, email, pwd)
-    values ($1,$2,$3,$4) returning *`;
-  const data = (await client.query(sql, [name, surname, email, pwd])).rows;
-  return data;
-}
-
 async function getDataByIdDB(id) {
   const client = await pool.connect();
   const sql = `select * from users where id=$1`;
@@ -22,32 +14,70 @@ async function getDataByIdDB(id) {
   return data;
 }
 
+async function createDataDB(name, surname, email, pwd) {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGEN');
+    const sql = `insert into users (name, surname, email, pwd)
+      values ($1,$2,$3,$4) returning *`;
+    const data = (await client.query(sql, [name, surname, email, pwd])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`createDataDB:${error.message}`);
+    return [];
+  }
+}
+
 async function updateDataDB(id, name, surname, email, pwd) {
   const client = await pool.connect();
-  const sql = `update users set name=$1, surname=$2, email=$3, pwd=$4
- where id=$5 returning *`;
-  const data = (await client.query(sql, [name, surname, email, pwd, id])).rows;
-  return data;
+  try {
+    await client.query('BEGEN');
+    const sql = `update users set name=$1, surname=$2, email=$3, pwd=$4
+   where id=$5 returning *`;
+    const data = (await client.query(sql, [name, surname, email, pwd, id])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`updateDataDB:${error.message}`);
+    return [];
+  }
 }
 
 async function patchDataDB(id, clientObj) {
   const client = await pool.connect();
-  const sql = `select * from users where id=$1`;
-  const oldObj = (await client.query(sql, [id])).rows;
-
-  const newObj = { ...oldObj[0], ...clientObj };
-
-  const sqlUpdate = `update users set name=$1, surname=$2, email=$3, pwd=$4
-     where id=$5 returning *`;
-  const result = (await client.query(sqlUpdate, [newObj.name, newObj.surname, newObj.email, newObj.pwd, id])).rows;
-  return result;
+  try {
+    await client.query('BEGEN');
+    const sql = `select * from users where id=$1`;
+    const oldObj = (await client.query(sql, [id])).rows;
+    const newObj = { ...oldObj[0], ...clientObj };
+    const sqlUpdate = `update users set name=$1, surname=$2, email=$3, pwd=$4
+       where id=$5 returning *`;
+    const result = (await client.query(sqlUpdate, [newObj.name, newObj.surname, newObj.email, newObj.pwd, id])).rows;
+    await client.query('COMMIT');
+    return result;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`patchDataDB:${error.message}`);
+    return [];
+  }
 }
 
 async function deleteDataDB(id) {
   const client = await pool.connect();
-  const sql = `delete from users where id=$1 returning *`;
-  const result = (await client.query(sql, [id])).rows;
-  return result;
+  try {
+    await client.query('BEGEN');
+    const sql = `delete from users where id=$1 returning *`;
+    const result = (await client.query(sql, [id])).rows;
+    await client.query('COMMIT');
+    return result;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`deleteDataDB:${error.message}`);
+    return [];
+  }
 }
 
 module.exports = {
